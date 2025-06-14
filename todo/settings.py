ce73e77772af88a15f4a589368d09f33ea2b9336
Het patel <hetbpatel203@gmail.com>
2025-06-14 19:19:36 +0530

    Setup for Render deployment

--- todo/settings.py
+++ todo/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
